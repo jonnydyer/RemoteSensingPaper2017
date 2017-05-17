c1b3d5eae355ab74302573a3c49e0762e789260e
Jonny Dyer <jonny@skyboximaging.com>
2017-05-17 15:10:23 -0700

    Added GIQE-5 based resolution

--- optimal_Q.py
+++ optimal_Q.py
@@ -28,83 +28,85 @@ def rer_approx(Q):
 def SNR_GIQE(Ne_well=10e3, Ne_rd=15.):
     return 0.07 * Ne_well / (np.sqrt(0.15 * Ne_well) + Ne_rd)
 
-snr0 = 30.
-rer0 = 0.3
-Fnum0 = 10.
-px_pitch0 = 6.5e-6
-d_ap0 = 0.35
-Ne_well0 = 30e3
-alt = 500e3
-lamda = 550e-9
-snr_exp = 1.         # 1 for stabilized systems or variable blur, 1.5 for constant blur
-gsd0 = px_pitch0 / (Fnum0*0.35) * alt
-NIIRS0 = giqe5(gsd0, rer0, snr0, 1.0)
-Q0 = Fnum0 * lamda / px_pitch0
+if __name__ == '__main__':
 
-lamda = 550e-9
-Fnum = Fnum0
-#d_ap = [0.2, 0.35, 0.5]
-Ne_well = [20e3, 30e3, 50e3]
-
-px_pitch = np.linspace(1e-6, 20e-6, 100)
-
-iq = np.zeros([len(px_pitch), len(Ne_well)])
-Q = np.zeros([len(px_pitch), len(Ne_well)])
-gsd = np.zeros([len(px_pitch), len(Ne_well)])
-
-for i, N_e in enumerate(Ne_well):
-    Q[:,i] = Fnum * lamda / px_pitch
-    rer = rer_approx(Q[:,i])
-    f = Fnum * d_ap0
-    gsd[:,i] = px_pitch / f * alt
-    #rer = rer0 * gsd[:,i] / gsd0
-    #snr = snr0 / (Q[:,i]/Q0)**snr_exp
-    snr = SNR_GIQE(px_pitch**2 / px_pitch0**2 * N_e)             
-    iq[:,i] = giqe5(gsd[:,i], rer, snr, 1.0) - NIIRS0
-
-#plt.figure(1)
-#plt.plot(px_pitch*1e6, Q)
-#plt.xlabel('Pixel Pitch (um)')
-#plt.ylabel('Q')
-#plt.legend(['Ne_well = %.2f m' % (N/1e3) for N in Ne_well])
-#
-#plt.figure(2)
-#plt.plot(px_pitch*1e6, gsd)
-#plt.xlabel('Pixel Pitch (um)')
-#plt.ylabel('GSD (m)')
-#plt.legend(['Ne_well = %.2f ke-' % (N/1e3) for N in Ne_well])
-#
-#plt.figure(3)
-#plt.plot(Q, gsd)
-#plt.xlabel('Q')
-#plt.ylabel('GSD (m)')
-#plt.legend(['Ne_well = %.2f ke-' % (N/1e3) for N in Ne_well])
-#plt.xlim(0.5, 3)
-#
-#plt.figure(4)
-#plt.plot(Q, snr)
-#plt.xlabel('Q')
-#plt.ylabel('SNR')
-#plt.legend(['Ne_well = %.2f ke-' % (N/1e3) for N in Ne_well])
-#plt.xlim(0.5, 3)
-
-f = plt.figure(5)
-plt.plot(Q, iq)
-#plt.vlines(Fnum0*lamda/px_pitch0, -0.8, 0.8, linestyle='--', 
-#           color='r', linewidth=1)
-plt.xlabel(r'Q $\left(\frac{\lambda F^\#}{p_{px}}\right)$')
-plt.ylabel('$\Delta$ NIIRS')
-plt.legend([r'$N_fNe_{e^-} = %.0f ke^-$' % (N/1e3) for N in Ne_well] +
-        ['SkySat-C'], loc='lower right')
-plt.xlim(0.5, 2)
-plt.ylim(-0.5, 0.6)
-f.set_tight_layout(True)
-f.savefig('figures/Q_iq.pgf')
-
-#f = plt.figure(6)
-#plt.plot(np.linspace(0.5, 4), rer_approx(np.linspace(0.5, 4)))
-#plt.xlabel('Q')
-#plt.ylabel('RER')
-
-
-plt.show()
+    snr0 = 30.
+    rer0 = 0.3
+    Fnum0 = 10.
+    px_pitch0 = 6.5e-6
+    d_ap0 = 0.35
+    Ne_well0 = 30e3
+    alt = 500e3
+    lamda = 550e-9
+    snr_exp = 1.         # 1 for stabilized systems or variable blur, 1.5 for constant blur
+    gsd0 = px_pitch0 / (Fnum0*0.35) * alt
+    NIIRS0 = giqe5(gsd0, rer0, snr0, 1.0)
+    Q0 = Fnum0 * lamda / px_pitch0
+    
+    lamda = 550e-9
+    Fnum = Fnum0
+    #d_ap = [0.2, 0.35, 0.5]
+    Ne_well = [20e3, 30e3, 50e3]
+    
+    px_pitch = np.linspace(1e-6, 20e-6, 100)
+    
+    iq = np.zeros([len(px_pitch), len(Ne_well)])
+    Q = np.zeros([len(px_pitch), len(Ne_well)])
+    gsd = np.zeros([len(px_pitch), len(Ne_well)])
+    
+    for i, N_e in enumerate(Ne_well):
+        Q[:,i] = Fnum * lamda / px_pitch
+        rer = rer_approx(Q[:,i])
+        f = Fnum * d_ap0
+        gsd[:,i] = px_pitch / f * alt
+        #rer = rer0 * gsd[:,i] / gsd0
+        #snr = snr0 / (Q[:,i]/Q0)**snr_exp
+        snr = SNR_GIQE(px_pitch**2 / px_pitch0**2 * N_e)             
+        iq[:,i] = giqe5(gsd[:,i], rer, snr, 1.0) - NIIRS0
+    
+    #plt.figure(1)
+    #plt.plot(px_pitch*1e6, Q)
+    #plt.xlabel('Pixel Pitch (um)')
+    #plt.ylabel('Q')
+    #plt.legend(['Ne_well = %.2f m' % (N/1e3) for N in Ne_well])
+    #
+    #plt.figure(2)
+    #plt.plot(px_pitch*1e6, gsd)
+    #plt.xlabel('Pixel Pitch (um)')
+    #plt.ylabel('GSD (m)')
+    #plt.legend(['Ne_well = %.2f ke-' % (N/1e3) for N in Ne_well])
+    #
+    #plt.figure(3)
+    #plt.plot(Q, gsd)
+    #plt.xlabel('Q')
+    #plt.ylabel('GSD (m)')
+    #plt.legend(['Ne_well = %.2f ke-' % (N/1e3) for N in Ne_well])
+    #plt.xlim(0.5, 3)
+    #
+    #plt.figure(4)
+    #plt.plot(Q, snr)
+    #plt.xlabel('Q')
+    #plt.ylabel('SNR')
+    #plt.legend(['Ne_well = %.2f ke-' % (N/1e3) for N in Ne_well])
+    #plt.xlim(0.5, 3)
+    
+    f = plt.figure(5)
+    plt.plot(Q, iq)
+    #plt.vlines(Fnum0*lamda/px_pitch0, -0.8, 0.8, linestyle='--', 
+    #           color='r', linewidth=1)
+    plt.xlabel(r'Q $\left(\frac{\lambda F^\#}{p_{px}}\right)$')
+    plt.ylabel('$\Delta$ NIIRS')
+    plt.legend([r'$N_fNe_{e^-} = %.0f ke^-$' % (N/1e3) for N in Ne_well] +
+            ['SkySat-C'], loc='lower right')
+    plt.xlim(0.5, 2)
+    plt.ylim(-0.5, 0.6)
+    f.set_tight_layout(True)
+    f.savefig('figures/Q_iq.pgf')
+    
+    #f = plt.figure(6)
+    #plt.plot(np.linspace(0.5, 4), rer_approx(np.linspace(0.5, 4)))
+    #plt.xlabel('Q')
+    #plt.ylabel('RER')
+    
+    
+    plt.show()
--- resolution.py
+++ resolution.py
@@ -2,6 +2,7 @@ from math import *
 import numpy as np
 from matplotlib import rc
 import matplotlib.pyplot as plt
+from optimal_Q import giqe5, rer_approx
 
 rc('figure', figsize=(3.5,2.5))
 rc('legend', fontsize='x-small')
@@ -14,17 +15,21 @@ Q = np.linspace(0.5, 2)
 D = 0.35
 lam = 550e-9
 h = 500e3
+snr0 = 20.
 
 alpha = alpha_eff(D, lam, Q)
 GRD = h * alpha
 GSD = h * lam / Q / D
+GRD1 = 10**((4.4 - giqe5(h * lam / D / Q, 
+                          rer_approx(Q), snr0/Q, 1.))/3.32)
 
 titl = "Effective resolution according to the GIQE\n model for D = %.2f m, \
 alt=%d km, " % (D, h/1e3) + \
 r"$\lambda$ = %d nm" % (lam * 1e9)
 
 f, ax1 = plt.subplots()
-ax1.plot(Q, GRD, label='GRD')
+ax1.plot(Q, GRD, label='GRD (approx)')
+ax1.plot(Q, GRD1, label='GRD (GIQE-5)')
 ax1.set_xlabel(r'Q$\left[\frac{\lambda F^\#}{p_{px}}\right]$')
 ax1.set_ylabel('(m)')
 ax1.set_ylim(0.35, 2)
