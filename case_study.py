7a30572a5e0a16739aa1657b6a4d2d65ac8c20f9
Jonny Dyer <jonny@skyboximaging.com>
2017-07-06 17:24:59 -0700

    Updated case_study.py

--- case_study.py
+++ case_study.py
@@ -55,35 +55,47 @@ def find_min_d(niirs_req, Q, snr_giqe, alt, obsc=0.3):
         niirs = giqe5(gsd, rer, snr_giqe, 1.0)
         return niirs-niirs_req
     #q_opt = minimize_scalar(f, method='bounded', bounds=[0.8, 2.0])
-    dmin = brentq(f, 0.3, 0.7)
+    dmin = brentq(f, 0.2, 0.9)
     #niirs_opt = f(dmin) + niirs_req
     gsd = alt * lam_c / Q / dmin
     return dmin, gsd
 
 systems = {
-    '50cm Resolution Point' : {
+    '25cm Resolution Point' : {
+        'grd_eff' : 0.3,
+        'snr_req' : 50.,
+        'Q' : 1.5,
+        'x_ct' : 7.5e3,      # m
+        'N_bands' : 5,
+        'p_px' : 5.5e-6,  # m'
+        'hpx_det' : 4608,
+        'vpx_det' : 2592.,
+        'Ne_FWC_det' : 40e3,
+        'rd_noise' : 5
+    },
+        '50cm Resolution Point' : {
         'grd_eff' : 0.5,
         'snr_req' : 70.,
         'Q' : 1.3,
-        'x_ct' : 8e3,      # m
+        'x_ct' : 7.5e3,      # m
         'N_bands' : 5,
-        'p_px' : 3.45e-6,  # m'
-        'hpx_det' : 4096,
-        'vpx_det' : 3000,
-        'Ne_FWC_det' : 10e3,
-        'rd_noise' : 3.5
+        'p_px' : 5.5e-6,  # m'
+        'hpx_det' : 4608,
+        'vpx_det' : 2592.,
+        'Ne_FWC_det' : 40e3,
+        'rd_noise' : 5
     },
     '1m Resolution Large Area' : {
         'grd_eff' : 1.0,
         'snr_req' : 70.,
-        'Q' : 1.0,
-        'x_ct' : 18e3,       # m
+        'Q' : 1.3,
+        'x_ct' : 20e3,       # m
         'N_bands' : 5,
-        'p_px' : 3.45e-6,    # m
-        'hpx_det' : 4096,
-        'vpx_det' : 3000,
-        'Ne_FWC_det' : 10e3,
-        'rd_noise' : 3.5
+        'p_px' : 4.5e-6,    # m
+        'hpx_det' : 5120,
+        'vpx_det' : 5120.,
+        'Ne_FWC_det' : 12e3,
+        'rd_noise' : 14.
     }
 }
 
@@ -101,8 +113,9 @@ for i, (n, s) in enumerate(systems.items()):
     hpx = s['x_ct'] / gsd
     psi_px = NsNe_fwc * hpx * Vgnd / gsd * s['N_bands'] # p_px
     f = s['p_px'] / gsd * alt
-    N_sensors = floor(hpx/s['hpx_det'])
-    FPS_req = psi_px / s['Ne_FWC_det'] / hpx / N_sensors / s['vpx_det']
+    N_sensors = ceil(hpx/s['hpx_det'])
+    FPS_req = ceil(psi_px / s['Ne_FWC_det'] / hpx / s['vpx_det'])
+    FPS_req = ceil(NsNe_fwc / s['Ne_FWC_det']) * Vgnd / gsd / s['vpx_det'] * s['N_bands']
     Ns = floor(gsd / Vgnd * s['vpx_det'] * FPS_req / s['N_bands'])
     DR = sqrt(Ns) * s['Ne_FWC_det'] / s['rd_noise']
     I_D_raw = Ns * np.log2(DR)
@@ -114,8 +127,10 @@ for i, (n, s) in enumerate(systems.items()):
     print '\t%0.1fmm focal plane width' % (hpx * s['p_px'] * 1e3)
     print '\t%d Cross-track pixels required' % hpx
     print '\t%d Cross track sensors' % (N_sensors)
+    print '\t%.3f km actual ct FOV' % (N_sensors * s['hpx_det'] * gsd / 1e3)
     print '\tRequired framerate %0.1f' % (FPS_req)
-    print '\tpsi_px required: %.1e' % (psi_px / N_sensors)
+    print '\tpsi_px  required: %.1e' % (psi_px)
+    print '\tpsi_px (per sensor) required: %.1e' % (psi_px / N_sensors)
     print '\tActual Ns : %d' % Ns
     print '\tI_D = %0.1f bit / pix raw' % (I_D_raw)
     print '\tDR = %.1f dB' % (20. * np.log10(DR))
